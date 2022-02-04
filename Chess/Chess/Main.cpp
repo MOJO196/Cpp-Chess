56f4789883cbfc12976f033d3998027e8b61b837
MOJO196 <movhet@gmail.com>
2022-02-04 11:33:12 +0100

    Chess

---
+++ Chess/Chess/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int16_t gameState[8][8] =
+{
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0},
+    {0, 0}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
